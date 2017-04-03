3155f4282628067f713b0ded740a09775ce97427
Patron <patroneq@gmail.com>
2017-04-03 21:37:49 +0200

    Utworzenie klasy testowej

---
+++ src/test/java/lab3_2/Tests.java
@@ -0,0 +1,15 @@
+package lab3_2;
+ 
+ import static org.junit.Assert.*;
+ 
+ import org.junit.Test;
+ import static org.powermock.api.mockito.PowerMockito.*;
+ 
+ public class Tests {
+ 
+ 	@Test
+ 	public void test() {
+ 		fail("Not yet implemented");
+ 	}
+ 
+ }
